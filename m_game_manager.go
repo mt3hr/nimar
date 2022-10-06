1ae205b1a4c35bba75e3fef2122d3dac9a31b73c
= <=>
2022-10-06 20:01:29 +0900

    配牌まで実装しました

--- m_game_manager.go
+++ m_game_manager.go
@@ -1,7 +1,11 @@
 // ˅
 package nimar
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // ˄
 
@@ -9,6 +13,10 @@ type MGameManager struct {
 	// ˅
 
 	// ˄
+
+	dealerPlayer *MPlayer
+
+	notDealerPlayer *MPlayer
 
 	table *MTable
 
@@ -94,6 +102,266 @@ func (m *MGameManager) ExecuteOperator(operator *Operator) error {
 
 	m.table.UpdateView()
 	return nil
+	// ˄
+}
+
+func (m *MGameManager) StartGame() {
+	// ˅
+	m.preparateGame()
+	m.initializeGame()
+	// ˄
+}
+
+func (m *MGameManager) preparateGame() {
+	// ˅
+	m.determinateDealer()
+	// ˄
+}
+
+func (m *MGameManager) resetGame() {
+	// ˅
+	m.table.player1.tsumoriTile = nil
+	m.table.player1.hand = []*MTile{}
+	m.table.player1.kawa = []*MTile{}
+	m.table.player1.openedTile1 = &MOpenedTiles{}
+	m.table.player1.openedTile2 = &MOpenedTiles{}
+	m.table.player1.openedTile3 = &MOpenedTiles{}
+	m.table.player1.openedTile4 = &MOpenedTiles{}
+
+	m.table.player2.tsumoriTile = nil
+	m.table.player2.hand = []*MTile{}
+	m.table.player2.kawa = []*MTile{}
+	m.table.player2.openedTile1 = &MOpenedTiles{}
+	m.table.player2.openedTile2 = &MOpenedTiles{}
+	m.table.player2.openedTile3 = &MOpenedTiles{}
+	m.table.player2.openedTile4 = &MOpenedTiles{}
+
+	m.table.tsumo.tiles = []*MTile{}
+	// ˄
+}
+
+func (m *MGameManager) initializeGame() {
+	// ˅
+	m.resetGame()
+	m.table.tsumo.tiles = m.generateTiles()
+	m.shuffleTiles(m.table.tsumo.tiles)
+	m.distributeTiles()
+	//TODO
+	// ˄
+}
+
+func (m *MGameManager) getDealerPlayer() *MPlayer {
+	// ˅
+	return m.dealerPlayer
+	// ˄
+}
+
+func (m *MGameManager) generateTiles() []*MTile {
+	// ˅
+	tiles := []*MTile{}
+
+	for i := 1; i <= 4; i++ {
+		// 索子
+		for j := 1; j <= 9; j++ {
+			id := ""
+			num := j
+			dora := false
+			suit := Suit_SOZU
+			akadora := false
+
+			id = fmt.Sprintf("%d%s%d", num, suit.ToString(), i)
+			if i == 1 && j == 5 {
+				akadora = true
+				id += "赤ドラ"
+			}
+
+			tiles = append(tiles, &MTile{
+				id:      id,
+				num:     num,
+				dora:    dora,
+				suit:    &suit,
+				akadora: akadora,
+			})
+		}
+		// 萬子
+		for j := 0; j < 2; j++ {
+			id := ""
+			num := 0
+			dora := false
+			suit := Suit_MANZU
+			akadora := false
+
+			if j == 0 {
+				num = 1
+			} else {
+				num = 9
+			}
+
+			id = fmt.Sprintf("%d%s%d", num, suit.ToString(), i)
+			if i == 1 && j == 5 {
+				akadora = true
+				id += "赤ドラ"
+			}
+
+			tiles = append(tiles, &MTile{
+				id:      id,
+				num:     num,
+				dora:    dora,
+				suit:    &suit,
+				akadora: akadora,
+			})
+
+		}
+		// 筒子
+		for j := 0; j < 2; j++ {
+			id := ""
+			num := 0
+			dora := false
+			suit := Suit_PINZU
+			akadora := false
+
+			if j == 0 {
+				num = 1
+			} else {
+				num = 9
+			}
+
+			id = fmt.Sprintf("%d%s%d", num, suit.ToString(), i)
+			if i == 1 && j == 5 {
+				akadora = true
+				id += "赤ドラ"
+			}
+
+			tiles = append(tiles, &MTile{
+				id:      id,
+				num:     num,
+				dora:    dora,
+				suit:    &suit,
+				akadora: akadora,
+			})
+		}
+
+		id := ""
+		num := 0
+		dora := false
+		akadora := false
+		suit := Suit_NONE
+
+		suit = Suit_TON
+		id = fmt.Sprintf("%s%d", suit.ToString(), i)
+		tiles = append(tiles, &MTile{
+			id:      id,
+			num:     num,
+			dora:    dora,
+			suit:    &suit,
+			akadora: akadora,
+		})
+
+		suit = Suit_NAN
+		id = fmt.Sprintf("%s%d", suit.ToString(), i)
+		tiles = append(tiles, &MTile{
+			id:      id,
+			num:     num,
+			dora:    dora,
+			suit:    &suit,
+			akadora: akadora,
+		})
+
+		suit = Suit_SHA
+		id = fmt.Sprintf("%s%d", suit.ToString(), i)
+		tiles = append(tiles, &MTile{
+			id:      id,
+			num:     num,
+			dora:    dora,
+			suit:    &suit,
+			akadora: akadora,
+		})
+
+		suit = Suit_PE
+		id = fmt.Sprintf("%s%d", suit.ToString(), i)
+		tiles = append(tiles, &MTile{
+			id:      id,
+			num:     num,
+			dora:    dora,
+			suit:    &suit,
+			akadora: akadora,
+		})
+
+		suit = Suit_HAKU
+		id = fmt.Sprintf("%s%d", suit.ToString(), i)
+		tiles = append(tiles, &MTile{
+			id:      id,
+			num:     num,
+			dora:    dora,
+			suit:    &suit,
+			akadora: akadora,
+		})
+
+		suit = Suit_HATSU
+		id = fmt.Sprintf("%s%d", suit.ToString(), i)
+		tiles = append(tiles, &MTile{
+			id:      id,
+			num:     num,
+			dora:    dora,
+			suit:    &suit,
+			akadora: akadora,
+		})
+
+		suit = Suit_CHUN
+		id = fmt.Sprintf("%s%d", suit.ToString(), i)
+		tiles = append(tiles, &MTile{
+			id:      id,
+			num:     num,
+			dora:    dora,
+			suit:    &suit,
+			akadora: akadora,
+		})
+	}
+	return tiles
+	// ˄
+}
+
+func (m *MGameManager) determinateDealer() {
+	// ˅
+	rand.Seed(time.Now().Unix())
+	random := rand.Intn(2)
+	if random == 0 {
+		m.dealerPlayer = m.table.player1
+		m.notDealerPlayer = m.table.player2
+	} else if random == 1 {
+		m.dealerPlayer = m.table.player2
+		m.notDealerPlayer = m.table.player1
+	}
+	// ˄
+}
+
+func (m *MGameManager) shuffleTiles(tiles []*MTile) {
+	// ˅
+	temp := &MTile{}
+	randomIndex := 1
+	for i := 0; i < len(tiles); i++ {
+		temp = tiles[i]
+		rand.Seed(time.Now().Unix())
+		randomIndex = rand.Intn(len(tiles))
+		tiles[i] = tiles[randomIndex]
+		tiles[randomIndex] = temp
+	}
+	// ˄
+}
+
+func (m *MGameManager) distributeTiles() {
+	// ˅
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 4; j++ {
+			m.dealerPlayer.hand = append(m.dealerPlayer.hand, m.table.tsumo.Pop())
+		}
+		for j := 0; j < 4; j++ {
+			m.notDealerPlayer.hand = append(m.notDealerPlayer.hand, m.table.tsumo.Pop())
+		}
+	}
+	m.dealerPlayer.hand = append(m.dealerPlayer.hand, m.table.tsumo.Pop())
+	m.notDealerPlayer.hand = append(m.notDealerPlayer.hand, m.table.tsumo.Pop())
+	m.dealerPlayer.hand = append(m.dealerPlayer.hand, m.table.tsumo.Pop())
 	// ˄
 }
 
